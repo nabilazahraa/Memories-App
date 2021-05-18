b5d03fff74b66fc759f24a7657ed0aabf1ecddea
Nabila Zahra <nabilazehra@hotmail.com>
2021-05-18 12:05:25 +0500

    Posts jsx

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
@@ -9,12 +9,21 @@ const Posts = () => {
   const classes = useStyles();
 
   console.log(posts);
-  return (
-    <div>
-      <h1>posts</h1>
-      <Post />
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
